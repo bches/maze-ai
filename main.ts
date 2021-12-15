d0e068cb34646dfe384f46a92b50ff52d4decdda
Brian Chesney <bpchesney@gmail.com>
2021-12-15 14:56:59 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    abcderghijklmnopqrstuvwxyz += 1
+})
+function forever2 () {
+    for (let index = 0; index < qwertyuiopasdfghjklzxcvbnm; index++) {
+        qwertyuiopasdfghjklzxcvbnm += 1
+    }
+}
+scene.onOverlapTile(SpriteKind.Player, tiles.util.arrow6, function (sprite, location) {
+    timer.after(300, function () {
+        abcderghijklmnopqrstuvwxyz = 4
+    })
+})
 info.onCountdownEnd(function () {
     mySprite.setImage(img`
         . . . . c c c b b b b b . . . . 
@@ -20,27 +33,80 @@ info.onCountdownEnd(function () {
     mySprite.sayText(":(", 2000, true)
     mySprite.destroy(effects.ashes, 2000)
 })
+scene.onOverlapTile(SpriteKind.Player, tiles.util.arrow7, function (sprite, location) {
+    timer.after(340, function () {
+        abcderghijklmnopqrstuvwxyz = 2
+    })
+})
+scene.onOverlapTile(SpriteKind.Player, tiles.util.arrow3, function (sprite, location) {
+    timer.after(300, function () {
+        abcderghijklmnopqrstuvwxyz = 1
+    })
+})
+let dabc = 0
+let xxxxx = 0
+let yeeeee = 0
+let qwertyuiopasdfghjklzxcvbnm = 0
+let abcderghijklmnopqrstuvwxyz = 0
 let mySprite: Sprite = null
+forever2()
 let mySprite2 = 99999
 info.startCountdown(60)
 tiles.setTilemap(tilemap`level1`)
 mySprite = sprites.create(img`
-    . . . . c c c b b b b b . . . . 
-    . . c c b 4 4 4 4 4 4 b b b . . 
-    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
-    . e 4 4 4 f 4 4 4 4 f 5 4 4 e . 
-    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-    e b b 4 4 f f 4 4 f f 4 4 4 b e 
-    . e b 4 4 4 f f f f 4 4 4 b e . 
-    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-    8 7 2 e e e e e e e e e e 2 7 8 
-    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-    e b e 8 8 c c 8 8 c c c 8 e b e 
-    e e b e c c e e e e e c e b e e 
-    . e e b b 4 4 4 4 4 4 4 4 e e . 
-    . . . c c c c c e e e e e . . . 
+    . . . . c c c b b b b b . . . 
+    . . c c b 4 4 4 4 4 4 b b b . 
+    . c c 4 4 4 4 4 5 4 4 4 4 b c 
+    . e 4 4 4 f 4 4 4 4 f 5 4 4 e 
+    . b 4 5 4 4 5 4 4 4 4 4 4 4 b 
+    . b 4 4 4 4 4 4 4 4 4 4 5 4 4 
+    . b b 4 4 f f 4 4 f f 4 4 4 b 
+    . e b 4 4 4 f f f f 4 4 4 b e 
+    . 7 e e b 4 4 4 4 4 4 b e e 6 
+    . 7 2 e e e e e e e e e e 2 7 
+    . 6 6 2 2 2 2 2 2 2 2 2 2 6 c 
+    . c 6 7 6 6 7 7 7 6 6 7 6 c c 
+    . b e 8 8 c c 8 8 c c c 8 e b 
+    . e b e c c e e e e e c e b e 
+    . e e b b 4 4 4 4 4 4 4 4 e e 
+    . . . c c c c c e e e e e . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
+forever(function () {
+    timer.throttle("action", 2500, function () {
+        yeeeee = mySprite.y
+        xxxxx = mySprite.x
+    })
+})
+forever(function () {
+    for (let index = 0; index <= 2; index++) {
+        if (index == 1) {
+            dabc = abcderghijklmnopqrstuvwxyz
+        }
+        timer.throttle("action", 1000, function () {
+            if (0 == 0) {
+                if (index == 2) {
+                    abcderghijklmnopqrstuvwxyz = randint(1, 4)
+                }
+            }
+        })
+    }
+})
+forever(function () {
+    if (abcderghijklmnopqrstuvwxyz == 5) {
+        abcderghijklmnopqrstuvwxyz = 1
+    }
+})
+forever(function () {
+    if (abcderghijklmnopqrstuvwxyz == 1) {
+        mySprite.x += 1
+    } else if (abcderghijklmnopqrstuvwxyz == 2) {
+        mySprite.y += 1
+    } else if (abcderghijklmnopqrstuvwxyz == 3) {
+        mySprite.y += -1
+    } else if (abcderghijklmnopqrstuvwxyz == 4) {
+        mySprite.x += -1
+    } else {
+        abcderghijklmnopqrstuvwxyz += 1
+    }
+})
